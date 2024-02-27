c0ae9d36e6eb01bb7979ec09d9530610bf1aed39
unknown <mubashir.khan5059@gmail.com>
2024-02-27 19:52:14 +0530

    first commit

---
+++ lib/app/modules/home/views/home_view.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/home_controller.dart';
+
+class HomeView extends GetView<HomeController> {
+  const HomeView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Morse Code Converter'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Column(
+          children: [
+            Text(
+              'Morse Code Converter is working',
+              style: TextStyle(fontSize: 20),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
